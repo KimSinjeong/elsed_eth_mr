aa00a504389b4ba64c74eca94e6943137415c8b0
Shinjeong Kim <shinjeong.kim.99@gmail.com>
2023-11-20 07:36:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ class CMakeBuild(build_ext):
         cmake_args += ['-DPYTHON_INCLUDE_DIR=' + sysconfig.get_path('include')]
         cmake_args += ['-DPYTHON_LIBRARY=' + sysconfig.get_config_var('LIBDIR')]
 
-        cmake_args += ['-DOpenCV_INCLUDE_DIRS=' + '/home/sjkim/Documents/MR/mr-library-demo/.buildozer/android/platform/build-arm64-v8a/build/other_builds/opencv/arm64-v8a__ndk_target_21/opencv/include']
+        #cmake_args += ['-DOpenCV_INCLUDE_DIRS=' + '/home/sjkim/Documents/MR/mr-library-demo/.buildozer/android/platform/build-arm64-v8a/build/other_builds/opencv/arm64-v8a__ndk_target_21/opencv/include']
+        cmake_args += ['-DOpenCV_INCLUDE_DIRS=' + '/usr/local/include/opencv4']
         cmake_args += ['-DOpenCV_LIBS=' + '/home/sjkim/Documents/MR/mr-library-demo/.buildozer/android/platform/build-arm64-v8a/build/libs_collections/mr-demo/arm64-v8a']
         cmake_args += ['-DOpenCV_FOUND=' + '1']
         
